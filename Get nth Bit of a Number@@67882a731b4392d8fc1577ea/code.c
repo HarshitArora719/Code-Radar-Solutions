962ba0490949efc3825bb869379e716e89f5c855
Harshit Arora <aroraluvi96@gmail.com>
2025-03-18 18:01:53 +0530

    Solution to Get nth Bit of a Number

--- Get nth Bit of a Number@@67882a731b4392d8fc1577ea/code.c
+++ Get nth Bit of a Number@@67882a731b4392d8fc1577ea/code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
     int a; 
-    scanf("%d",&a)
+    scanf("%d",&a);
     int n = 3; 
-    int bit = (number >> n) & 1;
+    int bit = (a >> n) & 1;
     printf("%d\n", bit);
     return 0;
 }
